bf515273906f1f173356b99751bb5e634973beb0
Laurent Rochette <lrochette@gmail.com>
2017-03-21 17:05:54 -0700

    Fix artifact link

--- dsl/procedures/gatherLogs/steps/summary.pl
+++ dsl/procedures/gatherLogs/steps/summary.pl
@@ -1,5 +1,4 @@
 $[/plugins[EC-Admin]project/scripts/perlheaderJSON]
 
-$ec->setProperty("/myJob/report-urls/logBundle", "/commander/jobSteps/$[/myjobStep/jobStepId]/./$[/myJob/packageNumber].zip");
-
-
+$ec->setProperty("/myJob/artifactsDirectory",".");
+$ec->setProperty("/myJob/report-urls/logBundle", "/commander/jobSteps/$[/myjobStep/jobStepId]/$[/myJob/packageNumber].zip");
